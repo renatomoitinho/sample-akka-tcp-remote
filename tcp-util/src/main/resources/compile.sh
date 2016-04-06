e4ae8d6d6a719c4f538b3560b0fe7ef0a483d08e
Renato Moitinho Dias <renatomoitinhodias@gmail.com>
2016-04-06 10:19:06 -0300

    Adicionando protobuf ao protocolo tcp

--- tcp-util/src/main/resources/compile.sh
+++ tcp-util/src/main/resources/compile.sh
@@ -1,4 +1,5 @@
-SRC_DIR=~/Desktop/oi/tcp/sample-akka-tcp-remote/tcp-util/src/main/resources
-DST_DIR=~/Desktop/oi/tcp/sample-akka-tcp-remote/tcp-util/src/main/java/model
+SRC_DIR=/Data/java/supra/tcp-sample/tcp-sample/tcp-util/src/main/resources
+#~/Desktop/oi/tcp/sample-akka-tcp-remote/tcp-util/src/main/resources
+DST_DIR=/Data/java/supra/tcp-sample/tcp-sample/tcp-util/src/main/java/model
 
-protoc -I=${SRC_DIR} --java_out=${DST_DIR} ${SRC_DIR}/sample.proto
+protoc -I=${SRC_DIR} --java_out=${DST_DIR} ${SRC_DIR}/ping.proto
